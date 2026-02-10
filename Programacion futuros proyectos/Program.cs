890800a4b761f195213534aff8e48066e1d2c45c
Marcos Eliú Cuevas Jiménez <68508925+MarprockLol777@users.noreply.github.com>
2026-02-10 17:23:47 -0400

    My first proyect :) test

--- Programacion futuros proyectos/Program.cs
+++ Programacion futuros proyectos/Program.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Programacion_futuros_proyectos
+namespace CadenaDeCaracteres1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            String nombre1, nombre2;
+            int edad1, edad2;
+            String linea;
+            Console.Write("Ingrese el nombre:");
+            nombre1 = Console.ReadLine();
+            Console.Write("Ingrese edad:");
+            linea = Console.ReadLine();
+            edad1 = int.Parse(linea);
+            Console.Write("Ingrese el nombre:");
+            nombre2 = Console.ReadLine();
+            Console.Write("Ingrese edad:");
+            linea = Console.ReadLine();
+            edad2 = int.Parse(linea);
+            Console.Write("La persona de mayor edad es:");
+            if (edad1 > edad2)
+            {
+                Console.Write(nombre1);
+            }
+            else
+            {
+                Console.Write(nombre2);
+            }
+            Console.ReadKey();
         }
     }
 }
