a7b18e0eb6db43db37a7fedfcee4726b41b76b5b
Marcos Eliú Cuevas Jiménez <68508925+MarprockLol777@users.noreply.github.com>
2026-02-13 20:31:09 -0400

    Homework my first program :)
    Homework about even and odd numbers done.

--- Programacion futuros proyectos/Program.cs
+++ Programacion futuros proyectos/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Programacion_futuros_proyectos
+namespace EvenOrOddProgram
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            // Declare a variable to store the number
+            int number;
+
+            // Ask the user to enter a number
+            Console.Write("Enter a number: ");
+
+            // Read the number from keyboard and convert it to integer
+            number = int.Parse(Console.ReadLine());
+
+            // Check if the number is even or odd
+            // If the remainder of division by 2 is 0, it is even
+            // If the remainder of division by 2 is 1, it is odd
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number is EVEN.");
+            }
+            else
+            {
+                Console.WriteLine("The number is ODD.");
+            }
+
+            // Wait for a key before closing the program
+            Console.ReadKey();
         }
     }
 }
